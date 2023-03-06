b143f5d2638ca171f40706787665ad3296a5f70f
Camilo Figueroa <cigotete@gmail.com>
2023-03-05 21:06:45 -0500

    Created Controller to Register user, and respective route definition.

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -15,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
