b0b0b48ae789ea48be15b3ae235254da147df7ba
Camilo Figueroa <cigotete@gmail.com>
2023-03-06 21:54:55 -0500

    Updated migrations. Updated/added slug field, and as unique.

--- database/migrations/2023_03_06_000002_create_posts_table.php
+++ database/migrations/2023_03_06_000002_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('extract');
             $table->text('body');
             $table->enum('status', [Post::BORRADOR, Post::PUBLICADO])
