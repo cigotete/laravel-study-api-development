e0faa2c962fe7ed4759bd3a70ebbbe0e44789b38
Camilo Figueroa <cigotete@gmail.com>
2023-03-06 21:58:19 -0500

    Created UserSeeder and PostSeeder to feed database.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Camilo Figueroa',
+            'email'=> 'example@gmail.com',
+            'password' => '12345678',
+        ]);
+
+        User::factory(99)->create();
+    }
+}
