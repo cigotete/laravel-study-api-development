cdd16219ba21fee3bfa1062c12ae044abebafba6
Camilo Figueroa <cigotete@gmail.com>
2023-03-06 21:56:29 -0500

    Fixed Tag model name used as parameter at Tag relation in Post model.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,7 +21,7 @@ class Post extends Model
     }
 
     public function tags() {
-        return $this->belongsToMany(Tags::class);
+        return $this->belongsToMany(Tag::class);
     }
 
     public function image() {
