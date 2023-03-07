c73f735fc19a8eb7cc21a19a2be7a55d49f8a719
Camilo Figueroa <cigotete@gmail.com>
2023-03-07 12:49:26 -0500

    Created Category controller methods. Basic stage.

---
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return $categories;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $request->validate([
+                'name' => 'required|max:255',
+                'slug' => 'required|max:255|unique:categories', //unique in table 'categories'
+            ]);
+
+            $category = Category::create($request->all());
+            return $category;
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            return Category::findOrFail($id);
+            //return $category; // Uses of Model Binding implies model as parameter.
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try {
+
+            $category = Category::findOrFail($id);
+            $request->validate([
+                'name' => 'required|max:255',
+                'slug' => 'required|max:255|unique:categories,slug,' . $category->id,
+            ]);
+            $category->update($request->all());
+
+            return response()->json([
+                'message' => 'Model updated successfully'
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+
+            $category = Category::findOrFail($id);
+            $category->delete($id);
+
+            return response()->json([
+                'message' => 'Model deleted successfully'
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred'], 500);
+        }
+    }
+}
