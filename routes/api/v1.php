1c3a6e72aa446df9cfd8c5f998ea24766f751d3f
Camilo Figueroa <cigotete@gmail.com>
2023-03-07 12:42:40 -0500

    Update route file to have Category endopoints.

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\RegisterController;
+use App\Http\Controllers\Api\v1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
+Route::apiResource('categories', CategoryController::class)->names('api.v1.register');
