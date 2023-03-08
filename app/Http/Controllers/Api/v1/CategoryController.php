7f0d057629fb4d88c1735dd731f78f627a36e297
Camilo Figueroa <cigotete@gmail.com>
2023-03-08 11:46:18 -0500

    Added sort functionality according 'sort' parameter and its value.

--- app/Http/Controllers/Api/v1/CategoryController.php
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -15,6 +15,7 @@ class CategoryController extends Controller
     {
         $categories = Category::included()
                         ->filter()
+                        ->sort()
                         ->get();
         return $categories;
     }
