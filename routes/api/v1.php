37ad84aa1ff7596a6adc756a49e1077044cb8581
Camilo Figueroa <cigotete@gmail.com>
2023-03-08 22:26:44 -0500

    Fixed Categories route name assigned.

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [RegisterController::class, 'store'])->name('api.v1.register');
-Route::apiResource('categories', CategoryController::class)->names('api.v1.register');
+Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
