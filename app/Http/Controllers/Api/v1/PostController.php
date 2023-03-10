bb7ccb416375f9ab5578ac0e493a54b27954f0cb
Camilo Figueroa <cigotete@gmail.com>
2023-03-09 20:21:42 -0500

    Updating PostController to use Passport auth for store, update and delete.

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -9,6 +9,11 @@ use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except(['index, show']);
+    }
     /**
      * Display a listing of the resource.
      */
