0a01f5ca375e1f4083541d7031ead0dba4c6164a
Camilo Figueroa <cigotete@gmail.com>
2023-04-14 21:00:16 -0500

    Added functionalities to delete, show and edit to Client Controller.

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -111,6 +111,99 @@
                 </x-slot>
             </x-form-section>
 
+            {{-- Modal Edit --}}
+        <x-dialog-modal modal="editForm.open">
+            <x-slot name="title">
+                Edit client
+            </x-slot>
+
+            <x-slot name="content">
+                <div class="space-y-6">
+
+                    <div v-if="editForm.errors.length > 0"
+                        class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+                        <strong class="font-bold">Whoops!</strong>
+                        <span>¡Something wrong!</span>
+
+                        <ul>
+                            <li v-for="error in editForm.errors">
+                                @{{ error }}
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="">
+
+                        <x-label>
+                            Name
+                        </x-label>
+
+                        <x-input v-model="editForm.name" type="text" class="w-full mt-1" />
+                    </div>
+
+                    <div class="">
+                        <x-label>
+                            Redirection URL
+                        </x-label>
+
+                        <x-input v-model="editForm.redirect" type="text" class="w-full mt-1" />
+                    </div>
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <button type="button"
+                    v-on:click="update()"
+                    v-bind:disabled="editForm.disabled"
+                    class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm disabled:opacity-50">
+                    Update
+                </button>
+
+
+
+                <button v-on:click="editForm.open = false" type="button"
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                    Cancel
+                </button>
+            </x-slot>
+
+        </x-dialog-modal>
+
+        {{-- Modal Show --}}
+        <x-dialog-modal modal="showClient.open">
+            <x-slot name="title">
+                Show credentials
+            </x-slot>
+
+            <x-slot name="content">
+                <div class="space-y-2">
+
+                    <p>
+                        <span class="font-semibold">CLIENT: </span>
+                        <span v-text="showClient.name"></span>
+                    </p>
+
+                    <p>
+                        <span class="font-semibold">CLIENT_ID: </span>
+                        <span v-text="showClient.id"></span>
+                    </p>
+
+                    <p>
+                        <span class="font-semibold">CLIENT_SECRET: </span>
+                        <span v-text="showClient.secret"></span>
+                    </p>
+
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <button v-on:click="showClient.open = false" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm disabled:opacity-50">
+                    Cancelar
+                </button>
+            </x-slot>
+
+        </x-dialog-modal>
+
         </x-container>
 
     </div>
@@ -123,9 +216,24 @@
                 data() {
                     return {
                         clients: [],
+                        showClient: {
+                            open: false,
+                            name: null,
+                            id: null,
+                            secret: null
+                        },
                         createForm: {
                             disabled: false,
                             errors: [],
+                            name: null,
+                            redirect: null,
+                        },
+                        editForm: {
+                            open: false,
+                            errors: [],
+                            disabled: false,
+                            errors: [],
+                            id: null,
                             name: null,
                             redirect: null,
                         }
@@ -140,6 +248,13 @@
                             .then(response => {
                                 this.clients = response.data
                             });
+                    },
+
+                    show(client){
+                        this.showClient.open = true;
+                        this.showClient.name = client.name;
+                        this.showClient.id = client.id;
+                        this.showClient.secret = client.secret;
                     },
 
                     store() {
@@ -158,6 +273,7 @@
                                     'success'
                                 );
 
+                                this.show(response.data);
                                 this.getClients();
                                 this.createForm.disabled = false;
 
@@ -165,6 +281,69 @@
                                 this.createForm.errors = Object.values(error.response.data.errors).flat();
                                 this.createForm.disabled = false;
                             })
+                    },
+
+                    edit(client){
+                        this.editForm.open = true;
+                        this.editForm.errors = [];
+
+                        this.editForm.id = client.id;
+                        this.editForm.name = client.name;
+                        this.editForm.redirect = client.redirect;
+                    },
+
+                    update(){
+                        this.editForm.disabled = true;
+
+                        axios.put('/oauth/clients/' + this.editForm.id, this.editForm)
+                            .then(response => {
+                                this.editForm.open = false;
+                                this.editForm.disabled = false;
+                                this.editForm.name = null;
+                                this.editForm.redirect = null;
+                                this.editForm.errors = [];
+
+                                Swal.fire(
+                                    'Updated!',
+                                    'Client updated successfully.',
+                                    'success'
+                                );
+
+                                this.getClients();
+
+                            }).catch(error => {
+
+                                this.editForm.errors = Object.values(error.response.data.errors).flat();
+
+                                this.editForm.disabled = false;
+
+                            })
+                    },
+
+                    destroy(client) {
+                        Swal.fire({
+                            title: 'Are you sure?',
+                            text: "You won't be able to revert this!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: 'Yes, delete it!'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+
+                                axios.delete('/oauth/clients/' + client.id)
+                                    .then(response => {
+                                        this.getClients();
+                                    });
+
+                                Swal.fire(
+                                    'Deleted!',
+                                    'Your file has been deleted.',
+                                    'success'
+                                )
+                            }
+                        })
                     }
                 }
             }).mount('#app');
